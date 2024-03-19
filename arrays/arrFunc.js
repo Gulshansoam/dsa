a37197b98d4cae1fb4475ce2f3ee01fd8f8be8c4
gulshan <gulshansoam317@gmail.com>
2024-03-19 12:52:43 +0530

    array method sort is added

--- arrays/arrFunc.js
+++ arrays/arrFunc.js
@@ -1,17 +1,39 @@
-const arr = [11, 9, 2, 3, 8];
+const arr = [11, 9, 2, 3, 8, 8, 11];
+
+const arrOfString = ["dhruv", "John", "doe", "annie"];
+
+// ***************sorting of array using sort method******************************//
+
+//++++++ Remember if you sort array of number then always use callback inside sort function (refer w3s school for why)+++++//
+function arrSortAscOrder(sortVal) {
+  return sortVal.sort((a, b) => a - b);
+}
+const num_Sorted_arr = arrSortAscOrder(arr);
+console.log(
+  num_Sorted_arr,
+  "sorting in ascending order of array of numbers using sort method"
+);
 
 // *****************************filter-method************************************** //
 
-function removeOdd(val) {
-  return val.filter((res) => res % 2 === 1);
-}
 function removeEven(val) {
   return val.filter((res) => res % 2 === 0);
 }
 let evenNumber = removeEven(arr);
-console.log(evenNumber, "even numbers");
+console.log(evenNumber, "even numbers using filter method");
+
+function removeOdd(val) {
+  return val.filter((res) => res % 2 === 1);
+}
 let oddNumber = removeOdd(arr);
-console.log(oddNumber, "odd numbers");
+console.log(oddNumber, "odd numbers using filter method");
+
+function removeDuplicate(duplicateArr) {
+  return duplicateArr.filter((res, i) => duplicateArr.indexOf(res) === i);
+}
+
+const num_Remove_Dups = removeDuplicate(arr);
+console.log(num_Remove_dups, "remove_Duplicate using filter method");
 
 // *****************************map-method************************************** //
 
@@ -20,5 +42,3 @@ function multiplyByTwo(val) {
 }
 let newArr = multiplyByTwo(arr);
 console.log(newArr);
-
-
--- callbacks/callbacks.js
+++ callbacks/callbacks.js
@@ -1,6 +1,5 @@
 function add(a, callback) {
   return a + callback();
 }
-
 let one = add(1, () => 2);
 console.log(one);
