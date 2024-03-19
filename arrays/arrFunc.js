fa0b764f7d9c8b7bd958b704a9c7edbc202901a6
gulshan <gulshansoam317@gmail.com>
2024-03-19 15:22:26 +0530

    array method sorting of array of string is added

--- arrays/arrFunc.js
+++ arrays/arrFunc.js
@@ -1,19 +1,46 @@
 const arr = [11, 9, 2, 3, 8, 8, 11];
 
-const arrOfString = ["dhruv", "John", "doe", "annie"];
+const arrOfString = ["dhruv", "john", "doe", "annie"];
 
 // ***************sorting of array using sort method******************************//
 
 //++++++ Remember if you sort array of number then always use callback inside sort function (refer w3s school for why)+++++//
-function arrSortAscOrder(sortVal) {
+function arrSortAscNumOrder(sortVal) {
   return sortVal.sort((a, b) => a - b);
 }
-const num_Sorted_arr = arrSortAscOrder(arr);
+const num_asc_sorted_arr = arrSortAscNumOrder(arr);
 console.log(
-  num_Sorted_arr,
+  num_asc_sorted_arr,
   "sorting in ascending order of array of numbers using sort method"
 );
 
+function arrSortDescNumOrder(descSortVal) {
+  return descSortVal.sort((a, b) => b - a);
+}
+const num_desc_sorted_arr = arrSortDescNumOrder(arr);
+console.log(
+  num_desc_sorted_arr,
+  "sorting in descending order of array of numbers using sort method"
+);
+
+// +++++++++++++++++++++++++++++++++++
+// sorting array of string using array method
+
+function strSortAscOrder(arrStr) {
+  return arrStr.sort((a, b) =>
+    a.localeCompare(b, "en", { sensitivity: "base" })
+  );
+}
+const sorted_asc_string_arr = strSortAscOrder(arrOfString);
+console.log(sorted_asc_string_arr, "Sorted array of string in ascending order");
+
+function strSortDescOrder(strArr) {
+  return strArr.sort((a, b) =>
+    b.localeCompare(a, "en", { sensitivity: "base" })
+  );
+}
+const sorted_desc_arr_str = strSortDescOrder(arrOfString);
+console.log(sorted_desc_arr_str, "Sorted array of string in descending order");
 // *****************************filter-method************************************** //
 
 function removeEven(val) {
@@ -32,8 +59,8 @@ function removeDuplicate(duplicateArr) {
   return duplicateArr.filter((res, i) => duplicateArr.indexOf(res) === i);
 }
 
-const num_Remove_Dups = removeDuplicate(arr);
-console.log(num_Remove_dups, "remove_Duplicate using filter method");
+const num_remove_dups = removeDuplicate(arr);
+console.log(num_remove_dups, "remove_Duplicate using filter method");
 
 // *****************************map-method************************************** //
 
